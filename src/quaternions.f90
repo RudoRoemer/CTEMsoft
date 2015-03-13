6054bbcd9aec5e97c7257c3d3c21d1c2e0bbde2b
Marc DeGraef <degraef@cmu.edu>
2015-03-13 17:25:59 -0400

    correction of typos in revised quat_mult routine !!!

--- src/quaternions.f90
+++ src/quaternions.f90
@@ -276,9 +276,9 @@ use constants
     real(kind=sgl)                      :: res(4)
 
     res = (/ x(1)*y(1) - x(2)*y(2) - x(3)*y(3) - x(4)*y(4), &
-             x(1)*y(2) + x(1)*y(2) - epsijk * ( x(3)*y(4) - x(4)*y(3) ), &
-             x(1)*y(3) + x(1)*y(3) - epsijk * ( x(4)*y(2) - x(2)*y(4) ), &
-             x(1)*y(4) + x(1)*y(4) - epsijk * ( x(2)*y(3) - x(3)*y(2) ) /)
+             x(1)*y(2) + x(2)*y(1) - epsijk * ( x(3)*y(4) - x(4)*y(3) ), &
+             x(1)*y(3) + x(3)*y(1) - epsijk * ( x(4)*y(2) - x(2)*y(4) ), &
+             x(1)*y(4) + x(4)*y(1) - epsijk * ( x(2)*y(3) - x(3)*y(2) ) /)
     
 end function quat_mult
 
@@ -304,9 +304,9 @@ use constants
     real(kind=dbl)                      :: res(4)
 
     res = (/ x(1)*y(1) - x(2)*y(2) - x(3)*y(3) - x(4)*y(4), &
-             x(1)*y(2) + x(1)*y(2) - epsijkd * ( x(3)*y(4) - x(4)*y(3) ), &
-             x(1)*y(3) + x(1)*y(3) - epsijkd * ( x(4)*y(2) - x(2)*y(4) ), &
-             x(1)*y(4) + x(1)*y(4) - epsijkd * ( x(2)*y(3) - x(3)*y(2) ) /)
+             x(1)*y(2) + x(2)*y(1) - epsijkd * ( x(3)*y(4) - x(4)*y(3) ), &
+             x(1)*y(3) + x(3)*y(1) - epsijkd * ( x(4)*y(2) - x(2)*y(4) ), &
+             x(1)*y(4) + x(4)*y(1) - epsijkd * ( x(2)*y(3) - x(3)*y(2) ) /)
 
 end function quat_mult_d
 
